f408962753b285a33fdd93afc2727d3156d84050
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-04 08:38:41 -0700

    betty checks passed

--- 0x00-python-hello_world/10-main.c
+++ 0x00-python-hello_world/10-main.c
@@ -29,13 +29,13 @@ int main(void)
 		printf("Linked list has no cycle\n");
 	else if (check_cycle(head) == 1)
 		printf("Linked list has a cycle\n");
-	
+
 	current = head;
 	for (i = 0; i < 4; i++)
 		current = current->next;
 	temp = current->next;
 	current->next = head;
-	if(check_cycle(head) == 0)
+	if (check_cycle(head) == 0)
 		printf("Linked list has no cycle\n");
 	else if (check_cycle(head) == 1)
 		printf("Linked list has a cycle\n");
