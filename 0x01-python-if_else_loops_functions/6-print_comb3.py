be61ce77da63c5c401dbb576387b343334022076
kipngeno <kipngenokevin254@gmail.com>
2023-10-07 13:18:52 +0300

    program that prints out all possible combinations of two numbers between 0 to 99

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(9):
+    for n in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, n), end="")
+        else:
+            print("{}{}".format(i, n), end=", ")
+print()
