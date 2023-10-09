a9e6d5519ef8408933e3368ef448e1c6bef14178
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-09 01:05:06 -0700

    This script will not run if imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 import sys
-j = 0
-total_arguments = len(sys.argv)
-if total_arguments == 1:
-    print("{} arguments.".format(total_arguments - 1))
-elif total_arguments == 2:
-    print("{} argument:".format(total_arguments - 1))
-else:
-    print("{} arguments:".format(total_arguments - 1))
-for i in sys.argv:
-    if j == 0:
+if __name__ == "__main__":
+    j = 0
+    total_arguments = len(sys.argv)
+    if total_arguments == 1:
+        print("{} arguments.".format(total_arguments - 1))
+    elif total_arguments == 2:
+        print("{} argument:".format(total_arguments - 1))
+    else:
+        print("{} arguments:".format(total_arguments - 1))
+    for i in sys.argv:
+        if j == 0:
+            j += 1
+            continue
+        print("{}: {}".format(j, sys.argv[j]))
         j += 1
-        continue
-    print("{}: {}".format(j, sys.argv[j]))
-    j += 1
