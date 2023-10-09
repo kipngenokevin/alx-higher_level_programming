d3ff90cb36aa5ac7d9b60861320e18a62082f5b0
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-09 02:56:35 -0700

    should print only names that do not start with __

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 if __name__ == "__main__":
     for i in dir(hidden_4):
-        if i[0] == "_":
+        if i[0:2] == "__":
             continue
         else:
             print("{}".format(i))
