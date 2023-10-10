05810b91bf313c45560748418eaa4ab1c71c64a4
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-10 15:17:41 -0700

    rewrote the whole code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,10 +1,10 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 /**
- * reverse_list - reverse the list
- * @head: pointer of the head element
- * Return: pointer to prev
+ * reverse_list - reverse list
+ * @head: pointer to filetype listint
+ * Return: previous
  */
 listint_t *reverse_list(listint_t *head)
 {
@@ -15,31 +15,28 @@ listint_t *reverse_list(listint_t *head)
 		next = current->next;
 		current->next = prev;
 		prev = current;
-		prev = current;
 		current = next;
 	}
 	return (prev);
 }
 /**
- * is_palindrome - check if it is palindrome
- * @head: pointer to pointer of head element
- * Return: 1 if palindrome 0 if not
+ * is_palindrome - checks if its palindrom
+ * @head: pointer to pointer of head
+ * Return: 0 or 1
  */
 int is_palindrome(listint_t **head)
 {
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-
 	listint_t *slow = *head, *fast = *head;
-	listint_t *second_half = NULL;
 
 	while (fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 	}
+	listint_t *second_half = reverse_list(slow);
 
-	second_half = reverse_list(slow);
 	while (second_half)
 	{
 		if ((*head)->n != second_half->n)
