2df139844c607e984ea69445aab0cabdc66a217e
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-11 16:00:39 -0700

    using sets instead of list

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,7 @@ def common_elements(set_1, set_2):
     if not set_1 or not set_2:
         return
     else:
-        c = list(set_1 & set_2)
+        a = set(set_1)
+        b = set(set_2)
+        c = (a & b)
         return c
