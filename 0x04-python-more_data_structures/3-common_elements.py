d74323ea8997b48bf3621618ab34f8b792f400e1
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-11 16:02:52 -0700

    removed the function checking whether ser is empty

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,5 @@
 def common_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return
-    else:
-        a = set(set_1)
-        b = set(set_2)
-        c = (a & b)
-        return c
+    a = set(set_1)
+    b = set(set_2)
+    c = (a & b)
+    return c
