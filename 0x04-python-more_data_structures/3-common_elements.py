a9e1291694b56993009dbb353d1b456365f6015c
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-11 15:55:00 -0700

    function that returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+def common_elements(set_1, set_2):
+    if not set_1 or not set_2:
+        return
+    else:
+        a = set(set_1)
+        b = set(set_2)
+        c = list(a & b)
+        return c
