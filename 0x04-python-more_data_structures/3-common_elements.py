bcc72e37889468a788d044e90434d93b0280feea
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-11 16:18:24 -0700

    new way of implementing search

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,5 +3,8 @@ def common_elements(set_1, set_2):
     if not set_1 or not set_2:
         return
     else:
-        common_set = set(set_1) & set(set_2)
+        common_set = set()
+        for element in set_1:
+            if element in set_2:
+                common_set.add(element)
         return common_set
