443b6e471084cbd606b320766b1fc03a5f1be016
Kipngeno Kevin <kipngenokevin254@gmail.com>
2023-11-01 16:26:11 +0000

    if width or height is equal to 0, perimeter is equal to 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -60,4 +60,7 @@ class Rectangle:
 
     def perimeter(self):
         """This funtion returns the perimeter of a rectangle"""
-        return 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
