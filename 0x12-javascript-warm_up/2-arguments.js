7caaf49bd43c78429f0cda762f20f30d89fdbd36
kipngeno kevin <kipngenokevin254@gmail.com>
2024-01-10 14:32:00 +0000

    refactored code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-let index = 0;
-argv.forEach((val) => {
-  index++;
-});
-if (index === 2) {
+if (argv.length === 2) {
   console.log('No argument');
-} else if (index === 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
