a80bf8ac45f6205ab97c0606b34476d3a15ccab9
kipngeno kevin <kipngenokevin254@gmail.com>
2024-01-13 09:46:01 +0000

    script that lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
             passwd=sys.argv[2],
             db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
